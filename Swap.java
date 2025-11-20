eead6c6cde36604acd1b0f1563f137cc00db6010
vishnujanumula <kiranavula857@gamil.com>
2025-11-20 14:59:47 +0530

    External exam

---
+++ Swap.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+public class Swap{
+void swapping(int a,int b){
+{
+int c;
+c=a;a=b;b=c;
+System.out.println("After Swapping");
+System.out.println("Value of a is " +a);
+System.out.println("Value of b is " +b);
+}
+
+public static void main(String args[]){
+Swap s = new Swap();
+int a=2,b=3;
+System.out.println("Before swapping");
+System.out.println("Value of a is" +a);
+System.out.println("value of b is "+b);
+s.swapping(a,b);
+}
+}
